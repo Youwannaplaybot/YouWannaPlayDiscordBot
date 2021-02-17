1e7e8c64cdc6450ae757216bcf24dcaa489c29fd
jakobneufeld <jakob.n.neufeld@gmail.com>
2021-02-17 12:28:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.audit_logs import _transform_owner_id
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged on as {0}!'.format(client.user))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "Hello!":
+        person = message.author.name
+        await message.channel.send( "Hello, @" + person  )
+        await message.add_reaction(str('1️⃣'))
+    if message.content == "u stink":
+        await message.channel.send("NO U")
+    if message.content.startswith('$kick'):
+        if message.author.guild_permissions.kick_members:
+            user = message.mentions[0]
+            await message.channel.send("Hi, you will be kicked " + user.name)
+
+            await message.guild.kick(user)  
+        else:
+            await message.channel.send("You can not ban people u nitwit")
+    if message.content.lower() == "give me owner":
+            role =  message.guild.get_role(810488947133251607)
+            dm = await  message.author.create_dm()
+            await dm.send(" YOU HAVE OWNER GOOD LUCK")
+            await message.author.add_roles(role)
+            await message.channel.send("You now sieze the power of the owner!")
+client.run('ODExMjU5ODk0OTc5NzU2MTU0.YCvmpw.xL8VoYSmFubB7r6qWgTyEjFgUmc') 
